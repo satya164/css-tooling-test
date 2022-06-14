25d3a276b86cc96ddc3fd835727cb0c0e29db835
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-06-15 01:05:25 +0200

    feat: style Button component with CSS modules

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import styles from './Button.module.css';
 
 type Props = React.ComponentPropsWithoutRef<'button'>;
 
 export function Button(props: Props) {
-  return <button {...props} />;
+  return <button {...props} className={styles.button} />;
 }
